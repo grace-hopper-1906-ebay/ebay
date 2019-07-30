f4d61043e6ce8d6d4be3a748a6db13667c07e665
dotsalot <lara.seren13@gmail.com>
2019-07-30 16:51:26 -0400

    first attempts at bootstrap

--- client/components/footer.js
+++ client/components/footer.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const Footer = () => (
-  <div>
+  <footer className="page-footer text-center font-small blue pt-4 bg-primary">
     <p>Images from Harry Potter Wiki Fandom</p>
-  </div>
+  </footer>
+  // // <!-- Footer -->
+  // <footer class="page-footer font-small blue bg-primary">
+
+  //   {/* <!-- Copyright --> */}
+  //   <div class="footer-copyright text-center py-3">© 2018 Copyright:
+  //     <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+  //   </div>
+  //   {/* <!-- Copyright --> */}
+
+  // </footer>
+  // // <!-- Footer -->
 )
 
 export default Footer
