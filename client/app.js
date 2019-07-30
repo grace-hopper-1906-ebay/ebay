e07ecdc2e6100524feec505e81e7db191d6b09e1
Rebecca Rossman <rrossman25@gmail.com>
2019-07-30 16:05:03 -0400

    home, navbar, and footer components

--- client/app.js
+++ client/app.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
-import {Navbar} from './components'
+import {Navbar, Footer} from './components'
 import Routes from './routes'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Routes />
+    <div className="main-container">
+      <div id="nav">
+        <Navbar />
+      </div>
+      <div id="main-body">
+        <Routes />
+      </div>
+      <div id="footer">
+        <Footer />
+      </div>
     </div>
   )
 }
---
+++ client/components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => (
+  <div>
+    <p>Images from Harry Potter Wiki Fandom</p>
+  </div>
+)
+
+export default Footer
