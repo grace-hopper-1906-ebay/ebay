c4d08a6d2405eabf551f8ea22badbe7836b6b0ab
dotsalot <lara.seren13@gmail.com>
2019-07-30 12:05:54 -0400

    updated single-product component to use redux

--- client/components/single-product.js
+++ client/components/single-product.js
@@ -1,52 +1,59 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
+import {fetchSingleProduct} from '../store/single-product'
 
 /**
  * COMPONENT
  */
-export const SingleProduct = props => {
-  // const {name, price, description, image} = props //product has name, price, description, image
-  const name = 'Elder Wand'
-  const price = '20'
-  const description = "it's powerful"
-  const image =
-    'https://shop.universalorlando.com/images/L-Interactive-Ollivanders-Elder-Wand-1272549.JPG'
-  return (
-    <div>
+class SingleProduct extends React.Component {
+  componentDidMount() {
+    this.props.fetchSingleProduct(this.props.match.params.id)
+  }
+
+  render() {
+    return (
       <div>
-        <img src={image} />
+        <div>
+          <img src={this.props.image} />
+        </div>
+        <div>
+          <h3>{this.props.name}</h3>
+          <p>{this.props.description}</p>
+          <p>${this.props.price}</p>
+          <button>Add To Cart</button>
+        </div>
       </div>
-      <div>
-        <h3>{name}</h3>
-        <p>{description}</p>
-        <p>${price}</p>
-        <button>Add To Cart</button>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 /**
  * CONTAINER
  */
-// const mapState = state => {
-//   return {
-//     name: state.product.name,
-//     price: state.product.price,
-//     description: state.product.description,
-//     image: state.product.image
-//   }
-// }
+const mapState = state => {
+  return {
+    name: state.singleProduct.name,
+    price: state.singleProduct.price,
+    description: state.singleProduct.description,
+    image: state.singleProduct.image
+  }
+}
 
-// export default connect(mapState)(SingleProduct)
+const mapDispatch = dispatch => {
+  return {
+    fetchSingleProduct: productId => dispatch(fetchSingleProduct(productId))
+  }
+}
+
+export default connect(mapState, mapDispatch)(SingleProduct)
 
 /**
  * PROP TYPES
  */
-// SingleProduct.propTypes = {
-//   name: PropTypes.string,
-//   price: PropTypes.number,
-//   description: PropTypes.string,
-//   image: PropTypes.string
-// }
+SingleProduct.propTypes = {
+  name: PropTypes.string,
+  price: PropTypes.number,
+  description: PropTypes.string,
+  image: PropTypes.string
+}
