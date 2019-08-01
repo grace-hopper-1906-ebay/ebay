5311b79f59bccdf6dd5a422ff22c0041a3e4cb2c
Rebecca Rossman <rrossman25@gmail.com>
2019-08-01 17:40:02 -0400

    carousel

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {Carousel} from 'react-bootstrap'
 
 const Main = () => {
   return (
-    <div className="home-container">
-      <div>
-        <img src="https://i.ebayimg.com/images/g/nm4AAOSwgGdZmoDQ/s-l300.jpg" />
-      </div>
-      <Link to="/products">Check out our Products!</Link>
-    </div>
+    <Carousel className="body">
+      <Carousel.Item>
+        <img src="https://i.imgur.com/6lu9uZp.png" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="https://i.imgur.com/v1lj54Y.png" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="https://i.imgur.com/bxnjxfd.png" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="https://i.imgur.com/7aKc1Zw.png" />
+      </Carousel.Item>
+    </Carousel>
   )
 }
 
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,43 +1,44 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {Navbar, Nav} from 'react-bootstrap'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <nav className="navbar navbar-fixed-top">
-    <div className="container-fluid">
-      <div className="navbar-header">
-        <img src="/Users/rir/Documents/Grace-Hopper/ebay/public/phoenix.jpg" />
-        <Link className="navbar-brand" to="/">
-          Helios Wands
-        </Link>
-      </div>
-      <div>
-        <Link to="/cart">
-          <img src="https://t3.ftcdn.net/jpg/01/28/03/88/160_F_128038885_VcHWsuiAblK57vjzUcDAuJsSfkcJWdTs.jpg" />
-        </Link>
-      </div>
-      <div>
-        <Link to="/products">Wands</Link>
-      </div>
-      {isLoggedIn ? (
-        <ul className="nav navbar-nav">
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </ul>
-      ) : (
-        <ul className="nav navbar-nav">
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </ul>
-      )}
-    </div>
-  </nav>
+const Navigation = ({handleClick, isLoggedIn}) => (
+  <Navbar collapseOnSelect expand="lg" static="top" bg="fire">
+    <Navbar.Brand>
+      <Link to="/">Helios Wands</Link>
+    </Navbar.Brand>
+    {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav>
+        <Navbar.Text>
+          <Link to="/cart">Cart</Link>
+        </Navbar.Text>
+        <Navbar.Text>
+          <Link to="/products">Wands</Link>
+        </Navbar.Text>
+        <Navbar.Text>
+          {isLoggedIn ? (
+            <ul className="nav navbar-nav">
+              {/* The navbar will show these links after you log in */}
+              <Link to="/home">Home</Link>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </ul>
+          ) : (
+            <ul className="nav navbar-nav">
+              {/* The navbar will show these links before you log in */}
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+            </ul>
+          )}
+        </Navbar.Text>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
 /**
@@ -57,12 +58,12 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(Navigation)
 
 /**
  * PROP TYPES
  */
-Navbar.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
+// Navbar.propTypes = {
+//   handleClick: PropTypes.func.isRequired,
+//   isLoggedIn: PropTypes.bool.isRequired
+// }
