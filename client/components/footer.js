5b290a3c896a63ea1259217355e9dd30106bf37e
Rebecca Rossman <rrossman25@gmail.com>
2019-08-06 10:32:52 -0400

    css

--- client/components/footer.js
+++ client/components/footer.js
@@ -2,20 +2,9 @@ import React from 'react'
 
 const Footer = () => (
   <footer id="footer">
-    <p>Images from Harry Potter Wiki Fandom</p>
+    <p>Images from Harry Potter Wiki Fandom and Google Images</p>
     <p>Terms and Conditions</p>
   </footer>
-  // // <!-- Footer -->
-  // <footer class="page-footer font-small blue bg-primary">
-
-  //   {/* <!-- Copyright --> */}
-  //   <div class="footer-copyright text-center py-3">© 2018 Copyright:
-  //     <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
-  //   </div>
-  //   {/* <!-- Copyright --> */}
-
-  // </footer>
-  // // <!-- Footer -->
 )
 
 export default Footer
